ddb644b2ef90a265531891c20d0f5a80b42e400e
md amir hossain jowel <mdamirhossainjowel@gmail.com>
2022-09-25 17:21:05 +0600

    database updated

--- src/component/Complaint.js
+++ src/component/Complaint.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+// import { useAuthState } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
-import auth from "../firebase.init";
+// import auth from "../firebase.init";
 
 const Complaint = () => {
-  const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);
   const {
     register,
+    refetch,
+    
+    formState: { isValid },
     handleSubmit,
-    refetch
-   
-  } = useForm();
-  const onSubmit = (data) => {
+  } = useForm({
+    mode: "onChange",
+  });
+  const onSubmit =  (data) => {
     fetch(`http://localhost:5000/complaints`, {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
       },
-    })
-      .then((res) => res.json())
-      .then((result) => console.log(result));
+    }).then((res) => res.json());
+
+    alert("Complaint Posted");
+    refetch()
     
   };
 
@@ -30,39 +34,38 @@ const Complaint = () => {
         <label htmlFor="my-modal-3" className="btn modal-button ">
           Post a Complaint?
         </label>
+        <div>
+          <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+          <div className="modal">
+            <div className="modal-box relative">
+              <label
+                htmlFor="my-modal-3"
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+              <h3 className="text-lg font-bold">
+                Complaint about your problem!
+              </h3>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  className="input input-bordered input-accent w-full h-40 max-w-lg mb-3"
+                  placeholder="Enter Your Complaint...."
+                  {...register("Complaint", { required: true })}
+                />
 
-        <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-        <div className="modal">
-          <div className="modal-box relative">
-            <label
-              htmlFor="my-modal-3"
-              className="btn btn-sm btn-circle absolute right-2 top-2"
-            >
-              ✕
-            </label>
-            <h3 className="text-lg font-bold">Complaint about your problem!</h3>
-            <form>
-              <input
-                className="input input-bordered input-accent w-full h-40 max-w-lg mb-3"
-                placeholder="Enter Your Complaint...."
-                {...register("Complaint", { required: true })}
-              />
-              <div className="modal-action justify-center">
-                <label
-                  htmlFor="my-modal-3"
-                  className=" btn btn-accent"
-    
-                  type="submit"
-                  onClick={handleSubmit(onSubmit)}
-                >
-                  POST
-                </label>
-              </div>
-            </form>
+                <div className="modal-action justify-center">
+                  <input
+                    className=" btn btn-accent modal-close"
+                    disabled={!isValid}
+                    type='submit'
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-
       <div className="overflow-x-auto mx-3">
         <table className="table w-full">
           <thead>
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAuthState, useUpdateProfile } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
